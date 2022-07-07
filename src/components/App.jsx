455fd79b9e89731d6b4e154243b6beea79a03356
mountaintiger55 <99082587+mountaintiger55@users.noreply.github.com>
2022-07-07 21:38:13 +0200

    v1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [isMouseOver, setMouseOver] = useState(false);
+
+  function changeColorOnMouseOver() {
+    setMouseOver(true);
+  }
+
+  function changeColorOnMouseOut() {
+    setMouseOver(false);
+  }
+
   return (
     <div className="container">
       <h1>Hello</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button
+        style={{ backgroundColor: isMouseOver ? "black" : "white" }}
+        onMouseOver={changeColorOnMouseOver}
+        onMouseOut={changeColorOnMouseOut}
+      >
+        Submit
+      </button>
     </div>
   );
 }
