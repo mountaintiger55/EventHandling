edc92db49061376191fe591270a56644bdf27b75
mountaintiger55 <99082587+mountaintiger55@users.noreply.github.com>
2022-07-07 22:06:07 +0200

    V2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 function App() {
   const [isMouseOver, setMouseOver] = useState(false);
+  const [name, setName] = useState("");
 
   function changeColorOnMouseOver() {
     setMouseOver(true);
@@ -11,10 +12,20 @@ function App() {
     setMouseOver(false);
   }
 
+  function handleChange(event) {
+    //console.log(event.target.value);
+    setName(event.target.value);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <input type="text" placeholder="What's your name?" />
+      <h1>Hello {name}</h1>
+      <input
+        onChange={handleChange}
+        type="text"
+        placeholder="What's your name?"
+        value={name}
+      />
       <button
         style={{ backgroundColor: isMouseOver ? "black" : "white" }}
         onMouseOver={changeColorOnMouseOver}
